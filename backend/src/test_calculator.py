cfc25b6037eadf559393bb3febd102b00ae28a52
Olg Muzalev <ruzrushitelyvselennoy@mail.ru>
2025-03-13 15:57:01 +0300

    backend multiplication test

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -9,6 +9,9 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
+    
+    def test_multiplication(self):
+        self.assertEqual(self.calculator.multiplication(1, 2), 2)
 
 
 if __name__ == "__main__":
