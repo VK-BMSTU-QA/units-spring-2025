0051b55c8ee3c7c3d73f656cb56afb1dd3478e42
Valentin <v_stremin@mail.ru>
2025-03-15 18:14:23 +0300

    Правки frontend

--- frontend/src/pages/__tests__/MainPage.test.tsx
+++ frontend/src/pages/__tests__/MainPage.test.tsx
@@ -1,5 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { MainPage } from '../MainPage';
+import '@testing-library/jest-dom';
+import { Product } from '../../types';
+import { useProducts } from '../../hooks';
 
 describe('test MainPage', () => {
     it('should return render snapshot', () => {
@@ -7,5 +10,23 @@ describe('test MainPage', () => {
         jest.setSystemTime(new Date('2025-03-14T00:00:00Z'));
         const r = render(<MainPage />);
         expect(r.asFragment()).toMatchSnapshot();
+    });
+    it('should return filtered products if categories selected', () => {
+        const mainPage = render(<MainPage />);
+        const products: Product[] = useProducts();
+        const electronicsCategories = mainPage.getAllByText('Электроника');
+        const electronicsBadge = electronicsCategories.find((el) =>
+            el.classList.contains('categories__badge')
+        );
+        if (!electronicsBadge) {
+            throw new Error('Cant find category: "Электроника"');
+        }
+
+        fireEvent.click(electronicsBadge);
+        products.forEach((product) => {
+            if (product.category === 'Электроника') {
+                expect(mainPage.getByText(product.name)).toBeInTheDocument();
+            }
+        });
     });
 });
