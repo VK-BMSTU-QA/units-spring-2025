a13b834cd1046dde2b71c46e171b9d0536089ec0
Valentin <v_stremin@mail.ru>
2025-03-15 18:20:57 +0300

    Правки 1

--- frontend/src/pages/__tests__/MainPage.test.tsx
+++ frontend/src/pages/__tests__/MainPage.test.tsx
@@ -6,8 +6,6 @@ import { useProducts } from '../../hooks';
 
 describe('test MainPage', () => {
     it('should return render snapshot', () => {
-        jest.useFakeTimers();
-        jest.setSystemTime(new Date('2025-03-14T00:00:00Z'));
         const r = render(<MainPage />);
         expect(r.asFragment()).toMatchSnapshot();
     });
