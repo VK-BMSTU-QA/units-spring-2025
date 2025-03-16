668fc04dab73a7839dd30bc12bcffebcd0c756e6
Кумова Ксения <k.kumova@vkteam.ru>
2025-03-16 03:32:39 +0300

    tests

--- frontend/src/pages/MainPage/MainPage.test.tsx
+++ frontend/src/pages/MainPage/MainPage.test.tsx
@@ -1,59 +1,80 @@
-import React from 'react';
-import * as currentTimeHook from '../../hooks/useCurrentTime';
-import * as productsHook from '../../hooks/useProducts';
-import { Category, Product } from '../../types';
 import { render, fireEvent } from '@testing-library/react';
 import { MainPage } from './MainPage';
+import { useProducts, useCurrentTime } from '../../hooks';
+
+jest.mock('../../hooks/useProducts', () => ({
+    useProducts: jest.fn(),
+}));
+
+jest.mock('../../hooks/useCurrentTime', () => ({
+    useCurrentTime: jest.fn(),
+}));
+
+jest.mock('../../components', () => ({
+    Categories: jest.fn(({ onCategoryClick }) => (
+        <div>
+            <button onClick={() => onCategoryClick()}>Электроника</button>
+            <button onClick={() => onCategoryClick()}>Для дома</button>
+            <button onClick={() => onCategoryClick()}>Одежда</button>
+        </div>
+    )),
+    ProductCard: jest.fn(({ name }) => <div>{name}</div>),
+}));
+
+const Products = [{
+    id: 1,
+    name: 'IPhone 14 Pro',
+    description: 'Latest iphone, buy it now',
+    price: 999,
+    priceSymbol: '$',
+    category: 'Электроника',
+    imgUrl: '/iphone.png',
+},
+{
+    id: 2,
+    name: 'Костюм гуся',
+    description: 'Запускаем гуся, работяги',
+    price: 1000,
+    priceSymbol: '₽',
+    category: 'Одежда',
+},
+{
+    id: 3,
+    name: 'Настольная лампа',
+    description: 'Говорят, что ее использовали в pixar',
+    price: 699,
+    category: 'Для дома',
+    imgUrl: '/lamp.png',
+},
+{
+    id: 4,
+    name: 'Принтер',
+    description: 'Незаменимая вещь для студента',
+    price: 7000,
+    category: 'Электроника',
+},
+] 
 
 describe('test main page component', () => {
     beforeAll(() => {
-        
-        jest.spyOn(currentTimeHook, 'useCurrentTime').mockReturnValue(
-            '00:00:00'
-        );
-        
-        jest.spyOn(productsHook, 'useProducts').mockReturnValue([
-            {
-                id: 1,
-                name: 'IPhone 14 Pro',
-                description: 'Latest iphone, buy it now',
-                price: 999,
-                priceSymbol: '$',
-                category: 'Электроника',
-                imgUrl: '/iphone.png',
-            },
-            {
-                id: 2,
-                name: 'Костюм гуся',
-                description: 'Запускаем гуся, работяги',
-                price: 1000,
-                priceSymbol: '₽',
-                category: 'Одежда',
-            },
-            {
-                id: 3,
-                name: 'Настольная лампа',
-                description: 'Говорят, что ее использовали в pixar',
-                price: 699,
-                category: 'Для дома',
-                imgUrl: '/lamp.png',
-            },
-            {
-                id: 4,
-                name: 'Принтер',
-                description: 'Незаменимая вещь для студента',
-                price: 7000,
-                category: 'Электроника',
-            },
-        ] as Product[]);
-    });
-
-    afterEach(() => {
-        jest.resetAllMocks();
+        (useProducts as jest.Mock).mockReturnValue(Products);
+        (useCurrentTime as jest.Mock).mockReturnValue('00:00');
     });
 
     it('should render correctly', () => {
         const rendered = render(<MainPage />);
-        expect(rendered.getByText('Принтер'));
+        expect(rendered.asFragment()).toMatchSnapshot();
+        });
+
+    it('filters products', () => {
+       const rendered = render(<MainPage />);
+
+        fireEvent.click(rendered.getByText('Электроника'));
+        fireEvent.click(rendered.getByText('Для дома'));
+
+        expect(rendered.queryByText('Костюм гуся')).toBeNull;
+        expect(rendered.queryByText('Настольная лампа')).toBe;
+        expect(rendered.queryByText('Принтер')).toBe;
+        expect(rendered.queryByText('IPhone 14 Pro')).toBe;
     });
 });
